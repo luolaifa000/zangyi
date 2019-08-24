436d83e7bfff2a10cd3ab1ecc6fa39cf5688ee03
luolaifa000 <18510023526@163.com>
2019-08-24 10:09:22 +0800

    socket

--- log/z_log.c
+++ log/z_log.c
@@ -10,18 +10,22 @@
 
 #include "../server/z_server.h"
 
+#include "../alloc/z_alloc.h"
+
 
 static loger *logger;
 
 int z_log_init(char *filename)
 {
+    logger = z_alloc(sizeof(*logger));
     logger->filename = filename;
     return Z_OK;
 }
 
 void z_log(const char *fmt, ...)
 {
-    
+    //printf("logger->filename = %s\n", logger->filename);
+    FILE *fp;
     size_t size, len;
     char buf[LOG_MAX_LEN];
     va_list args;
@@ -31,14 +35,14 @@ void z_log(const char *fmt, ...)
 time_t tv_sec; // seconds 
 long tv_usec; // microseconds 
 };*/
-
+    fp = fopen(logger->filename, "a+");
     len = 0;            
     size = LOG_MAX_LEN; 
 
     gettimeofday(&tv, NULL);
 
-    printf("timeval.tv.tv_sec:%d\n", tv.tv_sec);
-    printf("timeval.tv.tv_usec:%d\n", tv.tv_usec);
+    //printf("timeval.tv.tv_sec:%d\n", tv.tv_sec);
+    //printf("timeval.tv.tv_usec:%d\n", tv.tv_usec);
     //timeval.tv.tv_sec:1565621144   时间戳
     //timeval.tv.tv_usec:652879     微秒
     buf[len++] = '[';
@@ -57,8 +61,10 @@ long tv_usec; // microseconds
     buf[len++] = '\n';
 
     n = write(2, buf, len);
+    n = fwrite(buf, len, 1, fp);
     if (n < 0) {
         printf("write log error : %s\n", strerror(errno));
     }
+    fclose(fp);
 }
 
--- server/z_server.c
+++ server/z_server.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <getopt.h>
+#include <errno.h>
+
+#include <sys/socket.h>
+#include <netinet/in.h>
 
 #ifndef H_SERVER
 #define H_SERVER
@@ -23,31 +27,58 @@ int main(int argc, char *argv[])
     zconf *zf;
     zs = z_alloc(sizeof(*zs));
     zf = init_config(zf);
-    
     zs->zf = zf;
-    return_status = init_server_command(zs, argc, argv);
-
-    if (return_status != Z_OK) {
-        z_log("invalid option\n");
-        exit(Z_ERROR);
-    }
 
     return_status = z_log_init(zs->zf->logfile);
     
     if (return_status != Z_OK) {
-        z_log("invalid option\n");
+        z_log("z_log_init failure\n");
         exit(Z_ERROR);
     }
 
-    if (zs->port == 0 || zs->server_ip == NULL) {
-        z_log("invalid option\n");
+    return_status = init_server_command(zs, argc, argv);
+
+    if (return_status != Z_OK) {
+        z_log("invalid option1\n");
         exit(Z_ERROR);
     }
+
+    int sockedfd, status; 
+    struct sockaddr_in server_addr;  
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = inet_addr(zs->server_ip);
+    server_addr.sin_port = htons(zs->port);
+
+    sockedfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockedfd < 0) {
+        z_log("create socket error %s\n", strerror(errno));
+        return Z_ERROR;
+    } else {
+        z_log("create socket %d\n", sockedfd);
+    }
+    status = bind(sockedfd, (struct sockaddr *)&server_addr, sizeof(struct sockaddr));
+    if (status < 0) {
+        z_log("bind socket error %s\n", strerror(errno));
+        return Z_ERROR;
+    } else {
+        z_log("bind socket ok!\n");
+    }
+
+    status = listen(sockedfd, 5);
+    if (status < 0) {
+        z_log("listen socket error %s\n", strerror(errno));
+        return Z_ERROR;
+    } else {
+        z_log("listen socket ok!\n");
+    }
+
+    
     
 
-    printf("zs->port = %d\n", zs->port);
+    /*printf("zs->port = %d\n", zs->port);
     printf("zs->server_ip = %s\n", zs->server_ip);
-    printf("zs->zf->logfile = %s\n", zs->zf->logfile);
+    printf("zs->zf->logfile = %s\n", zs->zf->logfile);*/
 
     return Z_OK;
 }
@@ -68,11 +99,13 @@ int init_server_command(zserver *zs, int argc, char **argv)
                 zs->port = z_atoi(optarg, strlen(optarg));
                 break;
             default:
-                z_log("invalid option -- '%c'\n", opt);
                 return Z_ERROR;
                 break;
         }
        
+    }
+    if (zs->port == 0 || zs->server_ip == NULL) {
+        return Z_ERROR;
     }
     return Z_OK;
 }
